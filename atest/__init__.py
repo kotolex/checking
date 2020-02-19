43a08cb41de19861d0781ad50db18cd4ed270831
kotolex <lexman2@ya.ru>
2020-02-19 21:32:02 +0500

    Add TODOs

--- atest/__init__.py
+++ atest/__init__.py
@@ -1,6 +1,7 @@
 from .runner import start
 from .asserts import *
 from .annotations import *
+# TODO before, after, data-provider, enabled, timeout, suite, listener, logging, parallel(?)
 
 
 __all__ = ['start', 'equals', 'is_none', 'not_none', 'test', 'WrongAnnotationPlacement']
--- atest/annotations.py
+++ atest/annotations.py
@@ -12,6 +12,6 @@ class WrongAnnotationPlacement(BaseException):
 def test(func: FunctionType):
     if not inspect.isfunction(func) or str(signature(func)) != '()':
         raise WrongAnnotationPlacement(
-            "Annotation test must be used only with no-argument functions! Its not supposed to work with classes or class methods")
+            "Annotation 'test' must be used only with no-argument functions! Its not supposed to work with classes or class methods")
     _DICT[func.__module__].append(func)
     _MAIN.append(func)
