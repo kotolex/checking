5b9b501e98c11c12a32303b6fd56f25a0ff005ce
kotolex <lexman2@ya.ru>
2020-02-19 21:45:00 +0500

    Add todo's mention for TestClass, change getting function has args

--- atest/__init__.py
+++ atest/__init__.py
@@ -1,7 +1,7 @@
 from .runner import start
 from .asserts import *
 from .annotations import *
-# TODO before, after, data-provider, enabled, timeout, suite, listener, logging, parallel(?)
+# TODO TestClass, before, after, data-provider, enabled, timeout, suite, listener, logging, parallel(?)
 
 
 __all__ = ['start', 'equals', 'is_none', 'not_none', 'test', 'WrongAnnotationPlacement']
--- atest/annotations.py
+++ atest/annotations.py
@@ -10,7 +10,7 @@ class WrongAnnotationPlacement(BaseException):
 
 
 def test(func: FunctionType):
-    if not inspect.isfunction(func) or str(signature(func)) != '()':
+    if not inspect.isfunction(func) or signature(func).parameters:
         raise WrongAnnotationPlacement(
             "Annotation 'test' must be used only with no-argument functions! Its not supposed to work with classes or class methods")
     _DICT[func.__module__].append(func)
