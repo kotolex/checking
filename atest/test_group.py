811f77b2fb61f36ca8435eba018d258a9c0c0331
Kotomin Alexey <Kotomin.AV@parma.ru>
2020-03-04 09:27:04 +0500

    Add doc and tests for TestGroup

--- atest/test_group.py
+++ atest/test_group.py
@@ -5,34 +5,63 @@ from .test_case import TestCase
 
 
 class TestGroup(TestCase):
+    """
+    Класс тестового набора или группы тестов, представляет собой все тесты из одного модуля, или все тесты одной группы,
+    однако это не тест-сьют (набор всех наборов). Тесть-сьют может быть только один и содержит все наборы тестов,
+    тест-сьют всегда создается при старте тестирования. Если есть хоть 1 тест, то при старте будет создан тестовый набор,
+     который будет помещен в тест-сьют.
+    """
 
     def __init__(self, name: str):
         super().__init__(name)
+        # Список тестов, то есть объектов класса Test
         self.tests: List[Test] = []
+        # Список функций выполняемых перед каждым тестом
         self.before_all: List[Callable] = []
+        # Список функций выполняемых после каждого теста
         self.after_all: List[Callable] = []
+        # Словарь результатов прогона данного набора
         self.test_results: Dict[str, List[Test]] = {'success': [], 'broken': [], 'failed': [], 'ignored': []}
 
     def add_test(self, test: Test):
+        """
+        Добавление теста в набор, при этом он получает имя группы, списки предварительных и последующих функций
+        :param test: объект класса Test
+        :return: None
+        """
         test.group_name = self.name
-        for before in self.before_all:
-            test.add_before(before)
-        for after in self.after_all:
-            test.add_after(after)
+        test.before = self.before_all
+        test.after = self.after_all
         self.tests.append(test)
 
     def add_before_test(self, func: Callable):
+        """
+        Добавление еще одной функции в предварительный пул, для выполнения перед каждым тестом.
+        :param func: функция
+        :return: None
+        """
         self.before_all.append(func)
-        for test in self.tests:
-            test.add_before(func)
 
     def add_after_test(self, func: Callable):
+        """
+        Добавление еще одной функции в завершающий пул, для выполнения после каждого теста
+        :param func: функция
+        :return: None
+        """
         self.after_all.append(func)
-        for test in self.tests:
-            test.add_after(func)
 
     def add_result_to(self, test: Test, result: str = 'success'):
+        """
+        Добавления теста в соответствующий раздел словаря результатов
+        :param test: объект Test
+        :param result: результат, по-умолчанию успешен
+        :return: None
+        """
         self.test_results[result].append(test)
 
     def is_empty(self):
+        """
+        Пуст ли набор
+        :return: True если нет тестов в наборе
+        """
         return len(self.tests) == 0
--- test/classes_test.py
+++ test/classes_test.py
@@ -1,7 +1,10 @@
 from unittest import TestCase as TC
 from unittest import main
+from random import randint
+
 from atest.test_case import TestCase
 from atest.basic_test import Test
+from atest.test_group import TestGroup
 
 
 class TestClasses(TC):
@@ -42,6 +45,68 @@ class TestClasses(TC):
         before_count = TestClasses.count
         test.run()
         self.assertEqual(before_count + 1, TestClasses.count)
+
+    def test_init_for_TestGroup(self):
+        group = TestGroup('default')
+        self.assertFalse(any([group.after_all, group.before_all, group.after, group.before, group.always_run_after,
+                              group.is_before_failed]))
+        self.assertEqual({'success': [], 'broken': [], 'failed': [], 'ignored': []}, group.test_results)
+
+    def test_add_test_for_TestGroup(self):
+        group = TestGroup('default')
+        test = Test('name', self.fake_runner)
+        group.add_test(test)
+        self.assertEqual(test, group.tests[0])
+        self.assertEqual('default.name', str(test))
+        self.assertFalse(any([group.after_all, group.before_all, group.after, group.before, test.after, test.before]))
+
+    def test_add_test_to_group_with_before_test(self):
+        group = TestGroup('default')
+        group.add_before_test(TestClasses.fake_runner)
+        test = Test('name', print)
+        group.add_test(test)
+        self.assertEqual(TestClasses.fake_runner, test.before[0])
+
+    def test_add_test_to_group_with_after_test(self):
+        group = TestGroup('default')
+        group.add_after_test(TestClasses.fake_runner)
+        test = Test('name', print)
+        group.add_test(test)
+        self.assertEqual(TestClasses.fake_runner, test.after[0])
+
+    def test_add_test_and_add_before_test(self):
+        group = TestGroup('default')
+        test = Test('name', print)
+        group.add_test(test)
+        self.assertFalse(test.before)
+        group.add_before_test(TestClasses.fake_runner)
+        self.assertEqual(TestClasses.fake_runner, test.before[0])
+
+    def test_add_result_to_default(self):
+        group = TestGroup('default')
+        test = Test('name', print)
+        self.assertFalse(group.test_results['success'])
+        group.add_result_to(test)
+        self.assertEqual(1, len(group.test_results['success']))
+
+    def test_add_result_to_other(self):
+        group = TestGroup('default')
+        test = Test('name', print)
+        index = randint(1, 3)
+        name = list(group.test_results.keys())[index]
+        self.assertFalse(group.test_results[name])
+        group.add_result_to(test, name)
+        self.assertEqual(1, len(group.test_results[name]))
+
+    def test_is_empty_default(self):
+        group = TestGroup('default')
+        self.assertTrue(group.is_empty())
+
+    def test_is_empty_with_test(self):
+        group = TestGroup('default')
+        test = Test('name', print)
+        group.add_test(test)
+        self.assertFalse(group.is_empty())
 
 
 if __name__ == '__main__':
