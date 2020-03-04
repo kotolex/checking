594496a9d1f2dd480501ef2912870c06bee115ac
Kotomin Alexey <Kotomin.AV@parma.ru>
2020-03-04 08:50:15 +0500

    Add doc and tests for Test

--- atest/basic_test.py
+++ atest/basic_test.py
@@ -4,14 +4,26 @@ from .test_case import TestCase
 
 
 class Test(TestCase):
+    """
+    Класс представляющий тест, основная единица тестового прогона
+    """
 
     def __init__(self, name: str, test: Callable):
+        """
+        Инициализация объекта кроме имени требует ссылки на функцию, которая и является тестом
+        :param name: имя теста
+        :param test: функция, которая будет запущена при выполнении теста
+        """
         super().__init__(name)
         self.test = test
         self.group_name = '__main__'
 
     def run(self):
+        """
+        Запуск теста (функции, помеченной аннотацией test)
+        :return: None
+        """
         self.test()
 
     def __str__(self):
-        return f'{self.group_name}.{self.test.__name__}'
+        return f'{self.group_name}.{self.name}'
--- test/classes_test.py
+++ test/classes_test.py
@@ -1,9 +1,15 @@
 from unittest import TestCase as TC
 from unittest import main
 from atest.test_case import TestCase
+from atest.basic_test import Test
 
 
 class TestClasses(TC):
+    count = 0
+
+    @classmethod
+    def fake_runner(cls):
+        cls.count += 1
 
     def test_init_for_Test_Case(self):
         case = TestCase('default')
@@ -20,6 +26,22 @@ class TestClasses(TC):
         test_case.add_after(print)
         self.assertTrue(test_case.after)
         self.assertEqual(1, len(test_case.after))
+
+    def test_init_for_Test(self):
+        print_func = print
+        test = Test('default', print_func)
+        self.assertEqual(print_func, test.test)
+        self.assertEqual('__main__', test.group_name)
+
+    def test_str_for_Test(self):
+        test = Test('name', print)
+        self.assertEqual('__main__.name', str(test))
+
+    def test_run_for_Test(self):
+        test = Test('name', self.fake_runner)
+        before_count = TestClasses.count
+        test.run()
+        self.assertEqual(before_count + 1, TestClasses.count)
 
 
 if __name__ == '__main__':
