3e200179ec9ee37d7610d385c9ab6e65c31e9620
Kotomin Alexey <Kotomin.AV@parma.ru>
2020-03-16 16:19:06 +0500

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="checking",
-    version="0.1.5",
+    version="0.1.6",
     author="Lex Draven",
     author_email="lexman2@yandex.ru",
     description="A small library for testing your code",
