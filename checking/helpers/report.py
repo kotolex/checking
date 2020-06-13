c2c96aa0b1c6a064f40a2859da02cb6521f10216
kotolex <lexman2@ya.ru>
2020-06-13 13:04:13 +0500

    add js function for open and close block

--- checking/helpers/report.py
+++ checking/helpers/report.py
@@ -6,6 +6,17 @@ BASE = '''
   <head>
     <meta charset="utf-8">
     <title>Test Results for #suite_name</title>
+    <script>
+        function opclose(locator) {
+            return function () {
+                if (document.querySelector(locator).childNodes[3].style.display === 'none') {
+                    document.querySelector(locator).childNodes[3].style = 'display:block';
+                } else {
+                    document.querySelector(locator).childNodes[3].style = 'display:none';
+                }
+            }
+        }
+    </script>
   </head>
   <body>
     <h1>Info</h1>
@@ -24,11 +35,12 @@ def generate(file_name: str, test_suite: TestSuite):
                       f"<li>broken tests: {len(test_suite.broken())}</li>\n"
                       f"<li>ignored tests: {len(test_suite.ignored())}</li></ul>\n")
 
-    html_lines.append(f"<p>Success percent: {len(test_suite.success())/(test_suite.tests_count()/100):.4} %</p>\n")
+    html_lines.append(f"<p>Success percent: {len(test_suite.success()) / (test_suite.tests_count() / 100):.4} %</p>\n")
     html_lines.append(f"<p>Total time: {test_suite.suite_duration():.2} seconds</p>\n")
     if test_suite.is_empty():
         html_lines.append("<div id='empty'>Suite is empty! There are no tests!</div>\n")
     else:
+        count = 1
         html_lines.append('<h2>Statistics:</h2>\n')
         for group in test_suite.groups:
             group_time = sum(t.duration() for t in test_suite.groups.get(group).test_results)
@@ -37,8 +49,22 @@ def generate(file_name: str, test_suite: TestSuite):
                 time_ = test.duration()
                 if time_ < 0.01:
                     time_ = 0.0
-                html_lines.append(f"<li>Test '{test.name}': elapsed time {time_:.2} seconds, status <b>{test.status}</b></li>\n")
+                html_lines.append(
+                    f"<li id='id_{count}'>Test '{test.name}': elapsed time {time_:.2} seconds, status <b>{test.status}</b>\n"
+                    f"<div style='display: none;'>\n"
+                    f"<p>Description:'{test.description}'</p>"
+                    f"<p>Argument: {test.argument}</p>"
+                    f"<p>Attempt number: {test.retries}</p>"
+                    f"<p>Status: {test.status.upper()}</p>"
+                    f"<p>Duration: {time_:.3} seconds</p>"
+                    f"<p>Exception: {str(test.reason).replace('<', '&lt;')}</p>"
+                    f"</div>\n"
+                    f"</li>\n")
+                count += 1
             html_lines.append('</ol>\n')
+        for _ in range(1, count):
+            html_lines.append(f"<script>document.querySelector('#id_{_}').addEventListener('click', "
+                              f"opclose('#id_{_}'))</script>")
     html_lines.append('</body>\n</html>')
     with open(f'{file_name}.html', 'wt') as file:
         file.write(''.join(html_lines))
