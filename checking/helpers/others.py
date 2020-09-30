ad40071116301b608588cb572362bd04bdf59cdb
kotolex <lexman2@ya.ru>
2020-09-30 10:45:50 +0500

    Add diff message for equals

--- checking/helpers/others.py
+++ checking/helpers/others.py
@@ -1,6 +1,7 @@
 import glob
-from typing import Any
 from datetime import datetime
+from typing import Any, Iterable
+from collections.abc import Set, Mapping
 
 
 def short(obj: Any, width: int = 45) -> str:
@@ -69,5 +70,45 @@ def format_seconds(seconds: float) -> str:
     return f'{hours}{minutes} minute(s) and {seconds:.2f} seconds'
 
 
-if __name__ == '__main__':
-    print(format_seconds(4135))
+def diff(first: Any, second: Any) -> str:
+    """
+    Function to gets differs between objects for message in asserts. Objects must be the same type and has length.
+    Returns empty string if objects are the same, equal, not the same type, has no length, or not one of
+    (Mapping, Iterable, Set)
+    :param first: any object to compare
+    :param second: any object to compare
+    :return: string info about difference or empty if cant be checked
+    """
+    if type(first) != type(second) or first is second or first == second:
+        return ''
+    type_1 = type(first)
+    try:
+        len_first = len(first)
+        len_second = len(second)
+        if len_first != len_second:
+            return f'Length of "{short(first, 20)}"({type_1})={len_first} and length of "{short(second, 20)}"({type_1})={len_second}'
+    except TypeError:
+        return ''
+    if isinstance(first, Set):
+        return f'Different elements in sets: ({short(first ^ second)})'
+    if isinstance(first, Mapping):
+        for key, value in first.items():
+            if key not in second:
+                return f"Dict {short(second, 20)} has no key={key}, but it contains key(s)" \
+                       f" {short(set(second.keys() - set(first.keys())), 100)}"
+            if value != second[key]:
+                return f'Diff at element with key="{short(key)}"({type(key)}): ' \
+                       f'\n    first  value="{short(value)}"({type(value)}) ' \
+                       f'\n    second value="{short(second[key])}"({type(second[key])})'
+    if isinstance(first, Iterable):
+        iter_2 = iter(second)
+        for index, element in enumerate(first):
+            sec_element = next(iter_2)
+            if type(element)!=type(sec_element):
+                return f'Different types at element with index {index}:\n    ' \
+                       f'first  value="{short(element)}"{type(element)}\n    ' \
+                       f'second value="{short(sec_element)}"{type(sec_element)}'
+            if element!=sec_element:
+                return f'Diff at element with index {index}:\n    first  value="{short(element)}"{type(element)}\n    ' \
+                       f'second value="{short(sec_element)}"{type(sec_element)}'
+    return ''
--- tests/others_test.py
+++ tests/others_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from checking.helpers.others import fake
+from checking.helpers.others import fake, diff
 
 
 class OthersTest(TestCase):
@@ -10,6 +10,44 @@ class OthersTest(TestCase):
 
     def test_fake_returns_none_with_params(self):
         self.assertIsNone(fake(1, 2, 3))
+
+    def test_diff_types(self):
+        self.assertEqual('', diff('1', 1))
+
+    def test_diff_no_lens(self):
+        self.assertEqual('', diff((e for e in '1'), (e for e in '1')))
+
+    def test_diff_lengths(self):
+        self.assertEqual('Length of "12"(<class \'str\'>)=2 and length of "1"(<class \'str\'>)=1', diff('12', '1'))
+
+    def test_diff_sets(self):
+        self.assertEqual('Different elements in sets: ({1, 4})', diff({1, 2, 3}, {2, 3, 4}))
+
+    def test_diff_dicts(self):
+        self.assertEqual('''Diff at element with key="1"(<class 'int'>): 
+    first  value="1"(<class 'int'>) 
+    second value="2"(<class 'int'>)''', diff({1: 1}, {1: 2}))
+
+    def test_diff_dicts_keys(self):
+        self.assertEqual('''Dict {1: 1, 3: 3} has no key=2, but it contains key(s) {3}''',
+                         diff({1: 1, 2: 2}, {1: 1, 3: 3}))
+
+    def test_diff_iter_values(self):
+        self.assertEqual('''Diff at element with index 1:
+    first  value="2"<class 'int'>
+    second value="3"<class 'int'>''',diff([1,2],[1,3]))
+
+    def test_diff_iter_types(self):
+        self.assertEqual('''Different types at element with index 1:
+    first  value="2"<class 'int'>
+    second value="3"<class 'str'>''',diff([1,2],[1,'3']))
+
+    def test_diff_equal_obj(self):
+        self.assertEqual('', diff(1, 1))
+        self.assertEqual('', diff('1', '1'))
+        self.assertEqual('', diff({1, 2}, {1, 2}))
+        self.assertEqual('', diff([1, 2], [1, 2]))
+        self.assertEqual('', diff(None, None))
 
 
 if __name__ == '__main__':
