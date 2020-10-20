612a914832891a54d0dcffec7d59b621a2a6ffd0
igrekus <upload234@mail.ru>
2020-10-20 16:33:17 +0300

    FIX: CI

--- tests/others_test.py
+++ tests/others_test.py
@@ -18,29 +18,29 @@ class OthersTest(TestCase):
         self.assertEqual('', diff((e for e in '1'), (e for e in '1')))
 
     def test_diff_lengths(self):
-        self.assertEqual('Length of "12"(<class \'str\'>)=2 and length of "1"(<class \'str\'>)=1', diff('12', '1'))
+        self.assertEqual("Length of '12' <str> == 2 but length of '1' <str> == 1", diff('12', '1'))
 
     def test_diff_sets(self):
-        self.assertEqual('Different elements in sets: ({1, 4})', diff({1, 2, 3}, {2, 3, 4}))
+        self.assertEqual('Different elements in two sets: {1, 4}', diff({1, 2, 3}, {2, 3, 4}))
 
     def test_diff_dicts(self):
-        self.assertEqual('''Diff at element with key="1"(<class 'int'>): 
-    first  value="1"(<class 'int'>) 
-    second value="2"(<class 'int'>)''', diff({1: 1}, {1: 2}))
+        self.assertEqual('''Diff in entry under key='1' <int>:
+    first  value='1' <int>
+    second value='2' <int>''', diff({1: 1}, {1: 2}))
 
     def test_diff_dicts_keys(self):
-        self.assertEqual('''Dict {1: 1, 3: 3} has no key=2, but it contains key(s) {3}''',
+        self.assertEqual('''Dict {1: 1, 3: 3} has no key='2' <int>, but contains key(s) {3}''',
                          diff({1: 1, 2: 2}, {1: 1, 3: 3}))
 
     def test_diff_iter_values(self):
-        self.assertEqual('''Diff at element with index 1:
-    first  value="2"<class 'int'>
-    second value="3"<class 'int'>''',diff([1,2],[1,3]))
+        self.assertEqual('''Diff at element index 1:
+    first  value='2' <int>
+    second value='3' <int>''', diff([1, 2], [1, 3]))
 
     def test_diff_iter_types(self):
-        self.assertEqual('''Different types at element with index 1:
-    first  value="2"<class 'int'>
-    second value="3"<class 'str'>''',diff([1,2],[1,'3']))
+        self.assertEqual('''Different types at element index 1:
+    first  value='2' <int>
+    second value='3' <str>''', diff([1, 2], [1, '3']))
 
     def test_diff_equal_obj(self):
         self.assertEqual('', diff(1, 1))
