95bd82bb31c1e456b46a1fc0392ce6c327a9eef3
igrekus <upload234@mail.ru>
2020-10-27 17:35:13 +0300

    DOC: update docstrings

--- checking/annotations.py
+++ checking/annotations.py
@@ -17,31 +17,31 @@ def test(*args, enabled: bool = True, name: Optional[str] = None, description: O
          data_provider: Optional[str] = None, retries: int = 1, groups: Optional[Tuple[str]] = None,
          priority: int = 0, timeout: int = 0, only_if: Optional[Callable[[], bool]] = None):
     """
-    The annotation that marks a function in a module as a test, does not work with classes and class methods and with
-    functions, that take an argument (except using of data provider).
-    :param args: parameters, in which a function may come if the method is marked just with  @test.
-    :param enabled: is the flag of the active test, if False then the test does not fall into the run and all its other
-    settings are ignored.
-    :param name: the name of the test, but if there is no name, then the name is the function name
-    :param description: Test description, if None will be taken from function documentation. If there are description
-    and documentation, then the parameter has the advantage, to wit this is in use, otherwise, documentation will be
-    taken from documentation of the function (Test).
-    :param data_provider: is the string name of data provider, which is not need to be in current module with test, the
-    main is that it was found during assembling of test entities. If not found, the exception UnknownProviderName will
-    be raised.
-    :param retries: is the total amount of attempts of run test, this is the number of how many times the test will be
-    run again in case of errors. If the test is successful, no more attempts are made, fixtures before and after the
-    test are run just 1 time!
-    :param groups: is the list of group names, to which the test will be assigned, if it empty, group automatically
-    creates with the name of the module. This parameter allows to group tests from different modules to one run.
-    :param priority: is the priority, for organization of tests execution order. The greater is 0, as parameter high the
-    execution of the test will be done later.
-    :param timeout is the number of seconds during waiting the test ends. If the test had not end, then
-    TestBrokenException exception will raised, and the thread, in which the test is executing, will be interrupted.
-    Due to a possible memory leak, it should be used only when there is a special need.
-    :param only_if: accepts a function that will be called before the test starts and the test will only be launched if
-    it returns True. Should be taken to filter tests, for instance in relation to the operating system used.
+    Decorator, marks a function as a test. Does not work with classes, class methods and functions,
+    that take arguments, except when explicitly using a data provider.
+
+    :param args: handles the '@test' shorthand, holds the wrapped function reference
+    :param enabled: marks a test as active or inactive; if inactive, all other parameters are ignored
+    :param name: test name; if None, wrapped function name is used
+    :param description: test description; if None, wrapped function docstring is used with parameter taking precedence
+    :param data_provider: data provider name, must be resolved during the test suite bootstrap, raises otherwise
+    :param retries: number of retries on the test failure; no additional attempts are made on success
+    :param groups: list of test group names the marked test is assigned to; if None, the current module name is used;
+        use this parameter to allow tests defined in different modules to be executed in the same test run
+    :param priority: test priority, the higher the number, the later a test is executed;
+        use this parameter to control the test execution order
+    :param timeout: number of seconds to wait for a busy test to finish executing;
+        if timeout is reached, the thread executing the test is terminated and TestBrokenException is raised;
+        use sparingly due to possible memory leaks
+    :param only_if: callable predicate to evaluate before the test execution;
+        the test is executed only if the predicate evaluates to True;
+        use this parameter to filter tests on a condition, for example the type of OS
     :return: fake
+    :raise UnknownProviderName: if no corresponding data provider is found during the bootstrap
+    :raise TestBrokenException: if test timeout is reached
+    :raise ValueError:
+        1. if only_if parameter is present and is not a callable
+        2. if groups parameter is present and is not a tuple of strings
     """
     if not enabled:
         return fake
@@ -85,22 +85,23 @@ def test(*args, enabled: bool = True, name: Optional[str] = None, description: O
 def provider(*args, enabled: bool = True, name: Optional[str] = None, cached: bool = False,
              map_to_str: Callable[[Any], str] = str):
     """
-    The annotation that marks a data provider, that is, a function that supplies data to a test. Such a function should
-    return Iterable or Sequence, otherwise will be an error. It is not possible at compile time to determine if the
-    function returns the correct type, so an exception with the wrong type will be thrown at runtime. Exception tests
-    with such provider are added to ignored.
-    :param args: are parameters in which a function may come if the method is marked simply with @data
-    :param enabled: the flag of the active provider, if False, then it does not fall into the list of providers and all
-    its other settings are ignored
-    :param name: is the name, if not specified, then takes the name of the function. By this name, tests are searched by
-    the provider, therefore only unique names are allowed. Duplicate name throws DuplicateNameException
-    :param cached: flag to save all provider data in memory and use it on second use. Can be useful only when provider
-    used more than once in test-suite and you do not want to get data again from some source like filesystem or db.
-    WARNING! Cache use memory, so it can take a lot of it for big data volumes.
-    :param map_to_str: function-mapper to represent item of provider
+    Decorator, marks a function as data provider, which will supply data to a test.
+    Marked function must return an **Iterable** object
+    and typing error will be raised at runtime if the returned value is not an **Iterable**.
+    Any test using a rising provider will be marked as 'ignored'.
+
+    :param args: handles the '@provider' shorthand, holds the wrapped function reference
+    :param enabled: marks a provider as active or inactive; if inactive, all other parameters are ignored
+    :param name: provider name, maps providers to tests that use them, must be unique;
+        if None, wrapped function name is used
+    :param cached: toggles caching;
+        if enabled, all data a provider generates will be stored into memory upon the first provider call,
+        all subsequent calls will use the cached data to avoid repeating resource-intensive data fetches;
+        be wary of the memory consumption
+    :param map_to_str: callable attribute, must return a string representation of the data item
     :return: fake
-    :raise DuplicateProviderNameException if provider with such name is already exists
-    :raise WrongDecoratedObject if @data annotation used on function without return or yield statements
+    :raise DuplicateProviderNameException: if provider name is already registered
+    :raise WrongDecoratedObject: if decorator is used on a function without return or yield statements
     """
     if not enabled:
         return fake
@@ -127,10 +128,14 @@ def provider(*args, enabled: bool = True, name: Optional[str] = None, cached: bo
 
 def before(*args, group_name: Optional[str] = None):
     """
-    It marks the function as mandatory to run before each module/group test.
-    :param group_name: the name of the group before which test the function will be executed. If no group name is
-    specified, a group is automatically created with the module name. A function does not have to be in the same module
-    as the tests.
+    Decorator, marks a function as a mandatory part of each test in a group.
+    The marked function is executed once strictly before each test in the specified test group.
+    Use this decorator to setup specific repeating environment for each test in a group.
+
+    :param args: handles the '@before' shorthand, holds the wrapped function reference
+    :param group_name: the name of a test group test functions are bound to;
+        if None, a new group with the name of the current module is created;
+        decorated function does not have to be in the same module as the tests
     :return: fake
     """
 
@@ -147,11 +152,15 @@ def before(*args, group_name: Optional[str] = None):
 
 def after(*args, group_name: Optional[str] = None):
     """
-    It marks the function as mandatory to run after each module/group test. If there are functions running before the
-    test (@before) and they failed, then these functions will not start!
-    :param group_name: the name of the group after each test of which the function will be executed. If the group name
-    is not specified, a group with the module name is automatically created. A function does not have to be in the same
-    module as the tests.
+    Decorator, marks a function as a mandatory part of each test in a group.
+    The marked function is executed once strictly after each test in a group.
+    The marked function is not executed if corresponding **@before** fails.
+    Use this decorator to correctly tear down fixtures built for individual tests.
+
+    :param args: handles the '@after' shorthand, holds the wrapped function reference
+    :param group_name: the name of a test group test functions in question are bound to;
+        if None, a new group with the name of the current module is created;
+        decorated function does not have to be in the same module as the tests
     :return: fake
     """
 
@@ -168,12 +177,13 @@ def after(*args, group_name: Optional[str] = None):
 
 def before_group(*args, name: Optional[str] = None):
     """
-    Marks a function as a mandatory part of a test group bootstrap process.
-    The marked function is executed once strictly before any of the tests in the specified test group.
+    Decorator, marks a function as a mandatory part of a test group bootstrap process.
+    The marked function is executed once strictly before the first test in the specified test group.
     Use this decorator to build test fixtures for individual test groups.
 
+    :param args: handles the '@before_group' shorthand, holds the wrapped function reference
     :param name: the name of a module or a test group the function is executed in advance of,
-    if no name is specified, the name of the current module is used
+        if no name is specified, the name of the current module is used
     :return: fake
     """
 
@@ -190,16 +200,16 @@ def before_group(*args, name: Optional[str] = None):
 
 def after_group(*args, name: Optional[str] = None, always_run: bool = False):
     """
-    Marks a function as a mandatory part of a test group teardown process.
+    Decorator, marks a function as a mandatory part of a test group teardown process.
     The marked function is executed once strictly after all of the tests in a group have finished running.
-    The marked function is not executed if @before_group fails, unless always_run set to True.
+    The marked function is not executed if corresponding **@before_group** fails, unless always_run set to True.
     Use this decorator to correctly tear down fixtures built for individual test groups.
 
-    :param name: the name of a module or a test group the function is executed after
-    if no name is specified, the name of the current module is used
-    :param args: handles the call-less @after_group decorator shorthand, holds the wrapped function reference
-    :param always_run: if False, execute only if @before_group has finished successfully,
-    if True, force the function execution anyway
+    :param args: handles the '@after_group' shorthand, holds the wrapped function reference
+    :param name: the name of a module or a test group the function is executed after,
+        if no name is specified, the name of the current module is used
+    :param always_run: if False, execute only if corresponding '@before_group' has finished successfully,
+        if True, force the function execution anyway
     :return: fake
     """
 
@@ -218,8 +228,8 @@ def after_group(*args, name: Optional[str] = None, always_run: bool = False):
 
 def before_suite(func: Callable[[], None]):
     """
-    Marks a function as a mandatory part of the whole test suite bootstrap process.
-    The marked function is executed once strictly before any of the tests and functions, marked with @before_group.
+    Decorator, marks a function as a mandatory part of the whole test suite bootstrap process.
+    The marked function is executed once strictly before any of the tests and functions, marked with **@before_group**.
     Use this decorator to build test fixtures for the whole test suite.
 
     :param func: a callable object, must not take any arguments
@@ -231,15 +241,15 @@ def before_suite(func: Callable[[], None]):
 
 def after_suite(*args, always_run: bool = False):
     """
-    Marks a function as a mandatory part of a test suite teardown process.
+    Decorator, marks a function as a mandatory part of a test suite teardown process.
     The marked function is executed once strictly after all of the test in the suite have finished running and
-    all of the functions marked with @before_group are executed.
-    The marked function is not executed if @before_suite fails, unless always_run set to True.
+    all of the functions marked with **@before_group** are executed.
+    The marked function is not executed if **@before_suite** fails, unless always_run set to True.
     Use this decorator to correctly tear down fixtures built for the whole test suite.
 
-    :param args: handles the call-less @after_suite decorator shorthand, holds the wrapped function reference
-    :param always_run: if False, execute only if @before_suite has finished successfully,
-    if True, force the function execution anyway
+    :param args: handles the '@after_suite' decorator shorthand, holds the wrapped function reference
+    :param always_run: if False, execute only if corresponding '@before_suite' has finished successfully,
+        if True, force the function execution anyway
     :return: fake
     """
 
@@ -276,12 +286,13 @@ def _check_func_for_soft_assert(func):
 
 def __check_is_function_without_args(func: Callable, annotation_name: str):
     """
-    Checking that the annotation is above the function without arguments, it is not intended to use annotations with
-    classes and / or with their methods.
-    :param func: is the function to test
-    :param annotation_name: is the name of annotation (for errors)
+    Helper, checks whether a decorator marks a function with no arguments.
+    The library is designed to work with free functions, classes and methods are not supported.
+
+    :param func: function object to check
+    :param annotation_name: decorator name
     :return: None
-    :raise: WrongAnnotationPlacement
+    :raise WrongDecoratedObject: if marked function is not a free function with no arguments
     """
     if not isfunction(func) or signature(func).parameters:
         raise WrongDecoratedObject(
@@ -291,10 +302,12 @@ def __check_is_function_without_args(func: Callable, annotation_name: str):
 
 def __check_is_function_for_provider(func: Callable[[Any], None]):
     """
-    Check that the function is suitable to accept values (use a data provider), that is, it has exactly 1 argument.
-    :param func: is the function
+    Helper, checks whether a function is compliant with the data provider API, i.e.
+    takes exactly one argument.
+
+    :param func: function object to check
     :return: None
-    :raise WrongDecoratedObject
+    :raise WrongDecoratedObject if marked function is not a free function with exactly one argument
     """
     if not isfunction(func) or not signature(func).parameters:
         raise WrongDecoratedObject(f"Function '{func.__name__}' marked with data_provider has no argument!")
@@ -306,17 +319,22 @@ def __check_is_function_for_provider(func: Callable[[Any], None]):
 def DATA_FILE(file_path: str, name: Optional[str] = None, cached: bool = False, encoding: str = 'UTF-8',
               map_function: Optional[Callable] = None):
     """
-    Function to use text file as data provider for test. Reads file lazily, do not get it to memory.
-    The function name explicitly stays uppercase for user to pay attention to it.
-    User must call it at the global module namespace, but not at fixtures or in tests!
-    :param name: name of the data-provider for use it in test, if not specified the file_path be used as name
-    :param cached: flag to cache values for using it more than once
-    :param file_path: file name or path-to-file with name, it can be full or relative path, but it must be "visible"
-    (accessible from module, where it is declared)
-    :param encoding: encoding of the text file (default UTF-8)
-    :param map_function: function, which map line from text file
+    Convenience helper, simplifies usage of a text file as a data provider.
+    Lazy-reads the specified file.
+    Must be defined in the module namespace, not in a fixture or a test.
+    Note: uppercase name is left deliberately, to attract attention.
+
+    :param name: provider name, maps providers to tests that use them, must be unique;
+        if None, file_path argument is used;
+    :param cached: toggles caching;
+        if enabled, all data provider generates will be stored into memory upon the first provider call,
+        all subsequent calls will use the cached data to avoid repeating file reads;
+    :param file_path: path to a file including the file name,
+        relative paths are allowed but the file must be accessible from the current module directory
+    :param encoding: text file encoding, defaults to UTF-8
+    :param map_function: callable to execute against each read line
     :return: None
-    :raise FileNotFoundError if file is not exists!
+    :raise FileNotFoundError: if specified file does no exist
     """
 
     def wrapper():
@@ -339,13 +357,15 @@ def DATA_FILE(file_path: str, name: Optional[str] = None, cached: bool = False, 
 
 def CONTAINER(value: Union[Sequence, Iterable, Container], name: str = None, map_to_str: Callable[[Any], str] = str):
     """
-    Sugar for simplify providing data, use it when provider is simple and can be written in one-liner, like list
-    comprehension or generator expression.
-    The function name explicitly stays uppercase for user to pay attention to it!
-    User must call it at the global module namespace, but not at fixtures or in tests!
-    :param value: sequence/iterable or any object you can use with for
-    :param name: name for the provider, if empty then 'container' will be used as name
-    :param map_to_str: function-mapper to represent item of provider
+    Convenience helper, simplifies usage of a Python container types as a data provider,
+    e.g. Iterable literals, comprehensions and generators, etc.
+    Must be defined in the module namespace, not in a fixture or a test.
+    Note: uppercase name is left deliberately, to attract attention.
+
+    :param value: any object which supports the Python iterator protocol
+    :param name: provider name, maps providers to tests that use them, must be unique;
+        if None, 'container' is used;
+    :param map_to_str: callable attribute, returning a string representation of an object
     :return: None
     """
 
